f7ea4124a438f75fd5027192995448e8d031eb53
Rodrigo Muñoz <turboslashdeveloper@gmail.com>
2018-12-27 19:56:37 -0300

    Add project files.

---
+++ proyecto1/proyecto1/urls.py
@@ -0,0 +1,21 @@
+"""
+Definition of urls for proyecto1.
+"""
+
+from django.conf.urls import include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', proyecto1.views.home, name='home'),
+    # url(r'^proyecto1/', include('proyecto1.proyecto1.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+]
